7be7f341314910f8ec693a144124e0a8adc51b6c
Pacyx <471309445@qq.com>
2018-04-26 18:21:21 +0800

    feature add axios

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,6 +3,8 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
+const webpack = require('webpack')
+
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,54 @@
 <template>
   <div class="hello">
-    
+    axios_GET:
+    <!-- <input /> -->
+    <button @click="send">GET</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: "Welcome to Your Vue.js App"
+    };
+  },
+  methods: {
+    send() {
+      let that = this;
+      that.$axios
+        .get("http://192.168.13.236:5005/v1/getdiagdesc/肘关节疼痛", {
+          params: {
+            // ID: 12345
+          },
+          withCredentials: true, // default
+          headers:{'content-type': 'application/json' },
+          responseType: 'json'
+        })
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+      // that.$axios({
+      //     method: "get",
+      //     url: "http://localhost:3000/",
+      //     withCredentials: false, // default
+      //   })
+      //   .then(function(response) {
+      //     console.log(response);
+      //   });
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
